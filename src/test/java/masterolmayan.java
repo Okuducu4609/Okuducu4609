fd584ac1000dcd830c089ab0e350e5214677700a
Okuducu4609 <okuducu4609@gmail.com>
2021-12-19 02:22:58 +0100

    master disinda branch added

--- src/test/java/masterolmayan.java
+++ src/test/java/masterolmayan.java
@@ -1,5 +1,6 @@
 public class masterolmayan {
     public static void main(String[] args) {
         System.out.println("master olmayan bir branch");
+        System.out.println("tekrar master olmayan branch adde");
     }
 }
